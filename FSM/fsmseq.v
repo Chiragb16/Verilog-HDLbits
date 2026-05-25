9ba8ac0c9f7944d8eeb9e1fba12dd041540f2e0f
Chirag Bhalla <chiragbhalla86@gmail.com>
2026-05-25 12:00:58 +0530

    Added FSM question solutions

---
+++ FSM/fsmseq.v
@@ -0,0 +1,33 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous reset
+    input data,
+    output reg start_shifting);
+    parameter S0=0,S1=1,S2=2,S3=3,S4=4;
+    reg [2:0]state,next_state;
+    always @(posedge clk)begin
+        if(reset)
+            state<=S0;
+        else
+            state<=next_state;
+    end
+ 
+    
+    always @(*)begin
+        case(state)
+            S0:next_state=(data)?S1:S0;
+            S1:next_state=(data)?S2:S0;
+            S2:next_state=(data)?S2:S3;
+            S3:next_state=(data)?S4:S0;
+            S4:next_state=S4;
+            default:next_state=S0;
+        endcase
+    end
+    always @(*)begin
+        if(reset&clk)
+            start_shifting=0;
+        else
+            start_shifting=(state==S4);
+    end
+endmodule
+
